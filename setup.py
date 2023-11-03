80a246e7f89867427e89c47452794a5b30dfae10
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-11-03 17:40:59 -0400

    Specify encoding for opening files

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', 'r', encoding='utf-8') as f:
     license_ = f.read()
 
 setup(
