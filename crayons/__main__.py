61bbf14e7ef9f5fe1861956dea5c937489ffa6e7
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-11-03 13:42:39 -0400

    Create __main__.py

---
+++ crayons/__main__.py
@@ -0,0 +1,17 @@
+import random
+
+from . import fg, bg, effects, fg_rgb, bg_rgb
+
+print('''crayons.py
+v0.3.4
+
+This is a module to print text with color! See the demo below:
+''')
+
+for i in range(10):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    fg_rgb(r, g, b)
+    print('R: {}, G: {}, B: {}'.format(r, g, b))
